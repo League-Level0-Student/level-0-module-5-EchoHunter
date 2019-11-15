a8a54a41394efdcebbf9e43de29270ec697049ed
League Student <league@iMac-18.attlocal.net>
2019-11-14 18:28:53 -0800

    :ru:
    prim or not, fibonnacci

--- src/_01_prime_or_not/PriemERnort.java
+++ src/_01_prime_or_not/PriemERnort.java
@@ -1,7 +1,28 @@
 package _01_prime_or_not;
 
+import javax.swing.JOptionPane;
+
 public class PriemERnort {
-public static void main(String[] args) {
-	
-}
+	public static void main(String[] args) {
+boolean isPrime = true;
+		String numb = JOptionPane.showInputDialog("Give me a number");
+
+		int number = Integer.parseInt(numb);
+
+		for (int i = 2; i < number; i++) {
+
+			if (number % i == 0) {
+				JOptionPane.showMessageDialog(null, "This number is not prime");
+				isPrime =false;
+				break;
+			}
+
+						}
+		if(isPrime==true) {
+			JOptionPane.showMessageDialog(null, "This number is prime");
+
+
+	}
+	}
+
 }
---
+++ src/_02_fibonacci/Fibonacci.java
@@ -0,0 +1,30 @@
+package _02_fibonacci;
+
+public class Fibonacci {
+public static void main(String[] args) {
+	int f1 = 0;
+	int f2 = 1;
+int f3 =0;
+
+
+System.out.println(f1);
+System.out.println(f2);
+
+for (int i = 0; i < 12; i++) {
+f3=f1+f2;
+	
+System.out.println(f3);
+
+	
+	f1=f2;
+	f2=f3;
+   
+	
+	
+}
+
+
+
+
+}
+}
