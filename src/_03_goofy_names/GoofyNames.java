25615bfaa8599f7396bd453b0c82bf47c37249ad
League Student <league@iMac.attlocal.net>
2019-12-05 16:59:12 -0800

    Update GoofyNames.java

--- src/_03_goofy_names/GoofyNames.java
+++ src/_03_goofy_names/GoofyNames.java
@@ -8,54 +8,59 @@ import javax.swing.JOptionPane;
 
 public class GoofyNames {
 	static String goofyName;
-	static String goof ;
-	static String uppercase; 
+	static String goof;
+	static String uppercase;
 	static String Letter;
-	static int i =1;
-	static int q =2;
+	static int i = 1;
+	static int q = 2;
+
 	public static void main(String[] args) {
-	goofyName =	  JOptionPane.showInputDialog("What is your name?");
-		
+		goofyName = JOptionPane.showInputDialog("What is your name?");
+
 		// 1. Ask the user to enter their n
 		// 2. Print upper case name to the console using .toUpperCase()
-		//    Run your program to see that this works.
-	uppercase = goofyName.toUpperCase();
+		// Run your program to see that this works.
+		uppercase = goofyName.toUpperCase();
 
-	// 3. Loop through each character of the name (steps 4 - 6).
-		//    HINT: Use .length() to determine the number of characters in the String.
+		// 3. Loop through each character of the name (steps 4 - 6).
+		// HINT: Use .length() to determine the number of characters in the String.
 
-			// 4. Create a String variable to store the next character of the name
-			//    using .substring(start, end)
-			//    HINT: replace 'start' and 'end' to get String with character at i 
-			 Letter = uppercase.substring(i,q);
-			// 5. Use MODULO to set this variable to upper case for EVEN characters
-			//    and lower case for ODD characters.
-		
+		// 4. Create a String variable to store the next character of the name
+		// using .substring(start, end)
+		// HINT: replace 'start' and 'end' to get String with character at i
+		Letter = uppercase.substring(i, q);
+		// 5. Use MODULO to set this variable to upper case for EVEN characters
+		// and lower case for ODD characters.
+
 		for (int j = 0; j < goofyName.length(); j++) {
-			
-				
-			
-if(i % 2 == 0) {
-	Letter.toUpperCase();
 
-	i++;
-	q++;
-	Letter = uppercase.substring(i,q);
- goofyName = goofyName + Letter;
-}
+			if (i % 2 == 0) {
+				Letter.toUpperCase();
 
-else {
-goofyName = goofyName + Letter;
-i++;
-q++;
-}
-		
-		}	
-			// 6. ADD this String  (containing 1 char) to the goofyName String
+				i++;
+				q++;
+				Letter = uppercase.substring(i, q);
+				goofyName = goofyName + Letter;
+				if (q == goofyName.length()) {
+					break;
+				}
 
+			}
+
+			else {
+				goofyName = goofyName + Letter;
+				i++;
+				q++;
+				if (q == goofyName.length()) {
+					break;
+				}
+
+			}
+
+		}
+		// 6. ADD this String (containing 1 char) to the goofyName String
 
 		// 7. Use pop-up to show user their Goofy name
-JOptionPane.showMessageDialog(null, "your goofy name is " + goofyName);
+		JOptionPane.showMessageDialog(null, "your goofy name is " + goofyName);
 	}
 }
-
